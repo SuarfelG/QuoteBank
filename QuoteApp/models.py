9d9df56bfc92747bcfb494dd5c4a5fb77593fad1
Surafel Gebrmedhin <surafelgmedhin7@gmail.com>
2024-06-01 05:23:08 +0300

    Database Created For Authentication

--- QuoteApp/models.py
+++ QuoteApp/models.py
@@ -7,3 +7,13 @@ class Quotes(db.Model,UserMixin):
     id=db.Column(db.Integer , primary_key=True)
     Author=db.Column(db.String(50) ,nullable=False)
     Quote=db.Column(db.String(50) ,nullable=False)
+
+
+class Authentication(db.Model,UserMixin):
+    __tablename__="Authentication"
+
+    id=db.Column(db.Integer , primary_key=True)
+    First_name=db.Column(db.String(50) ,nullable=False)
+    Last_name=db.Column(db.String(50) ,nullable=False)
+    Email=db.Column(db.String(50) ,nullable=False,unique=True)
+    password=db.Column(db.String(50) ,nullable=False,unique=True)
