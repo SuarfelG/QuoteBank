8e4106d0b77467f55c8570df5d3e200f49bac820
Surafel Gebrmedhin <surafelgmedhin7@gmail.com>
2024-06-17 17:20:19 +0300

    Back To Mysql

--- QuoteApp/QuoteManager.py
+++ QuoteApp/QuoteManager.py
@@ -11,6 +11,7 @@ def AddQuotes():
     if request.method=="POST":
             Quote=request.form.get("Quote")
             Author=request.form.get("author")
+            user_id=current_user.id
             check= Quotes.query.all()
             for x in check:
                   print(x.Quote)
@@ -18,7 +19,7 @@ def AddQuotes():
                         flash("Quote Already Exists" , category="error")
                         return redirect (url_for("view.SearchQuoteById"))
                   
-            newquote=Quotes( Author=Author,Quote=Quote )
+            newquote=Quotes( Author=Author,Quote=Quote , user_id=user_id)
             db.session.add(newquote)
             db.session.commit()
             flash("Quote Added Successfuly" ,category="success")
